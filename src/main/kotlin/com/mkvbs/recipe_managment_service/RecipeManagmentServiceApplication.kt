c9127030156dac3966da2b318fe2c7d727cb77b1
Maciej Kubis <77754368+mkubis00@users.noreply.github.com>
2026-01-06 20:36:19 +0100

    Initial commit
    Signed-off-by: mkvbs <maciej.k-2000@outlook.com>

---
+++ src/main/kotlin/com/mkvbs/recipe_managment_service/RecipeManagmentServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.mkvbs.recipe_managment_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecipeManagmentServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<RecipeManagmentServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/mkvbs/recipe_managment_service/RecipeManagmentServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mkvbs.recipe_managment_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecipeManagmentServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
